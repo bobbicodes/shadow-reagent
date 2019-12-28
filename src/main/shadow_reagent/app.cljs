2db3eb14b83a35337fcab5dea96698666a26f4c7
Bobby Towers <porkostomus@mail.com>
2019-12-27 18:34:59 -0800

    add hot reload

--- src/main/shadow_reagent/app.cljs
+++ src/main/shadow_reagent/app.cljs
@@ -5,6 +5,17 @@
   [:div#app
    [:h1 "shadow-cljs reagent template"]])
 
-(defn init []
+(defn render []
   (r/render [app]
             (.getElementById js/document "root")))
+
+(defn ^:dev/after-load start []
+  (render)
+  (js/console.log "start"))
+
+(defn ^:export init []
+  (js/console.log "init")
+  (start))
+
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
